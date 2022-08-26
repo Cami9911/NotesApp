b126d5964771ac06af36d9eb25554157e2940eb0
Tirnovan Camelia <c.tirnovan@viamedici.de>
2022-08-26 16:17:35 +0300

    add new note modal update

--- main.js
+++ main.js
@@ -23,3 +23,51 @@ function openNav() {
       document.getElementById("sidebarOption"+i).style.paddingLeft = "2px";
     }
   }
+
+  function hideIcon() {
+    document.getElementById("searchIcon").style.display = "none";
+  }
+
+  function displayIcon() {
+    document.getElementById("searchIcon").style.display = "block";
+  }
+
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  document.getElementById("defaultOpen").click();
+
+  var modal = document.getElementById("addNewNoteModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("addNewNoteBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeModal")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
